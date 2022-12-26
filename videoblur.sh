1aac0faa8e1688ca8ce2f293ba3155fdfb29c4c7
Peter Rigole <peter.rigole@atomicbits.io>
2022-12-26 22:40:35 +0100

    simplifying the dockerfile?

--- videoblur.sh
+++ videoblur.sh
@@ -3,4 +3,4 @@
 # echo "python path: $PYTHONPATH"
 # echo "list: $(ls -lha $HOME/Pytorch_faceblur)"
 
-python $HOME/Pytorch_faceblur/video.py -m /weights/Resnet50_Final.pth  "$@" 
+python /app/Pytorch_faceblur/video.py -m /weights/Resnet50_Final.pth  "$@" 
